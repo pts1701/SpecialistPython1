9d3abf43c4dd2e50430ee10e9f5c6d8dffd860ab
pts1701 <110337313+pts1701@users.noreply.github.com>
2022-08-08 13:51:30 +0300

    Update 04_hw.py

--- Module3/home_work/04_hw.py
+++ Module3/home_work/04_hw.py
@@ -5,3 +5,31 @@
 # Пример:
 # Дано: [2, -5, 8, 9, -25, 25, 4]
 # Результат: [3, 5, 2]
+
+import random
+import math
+n = int(input('Input number of random digits: '))
+numbers = []
+for i in range(0, n):
+    i = random.randint(-100, 100)
+    numbers.append(i)
+print("input= ", numbers) 
+
+even_positive = []
+for i in numbers:
+    if i > 0 and i % 2 ==0:
+        even_positive.append(i)
+
+print("All even positive =", even_positive)
+
+summa = sum(even_positive)
+
+print("Sum of all positives = ", summa)
+
+square_list = []
+for n in numbers:
+    if n > 0:
+        x = n**0.5
+        if x % 1 == 0:
+            square_list.append(int(x))
+print("Squares:  ", square_list)
