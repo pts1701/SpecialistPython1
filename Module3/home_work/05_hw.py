3eb2b71cbe6c815af57afb29b1c3dd83dcd80a33
pts1701 <110337313+pts1701@users.noreply.github.com>
2022-08-08 18:59:10 +0300

    Update 05_hw.py

--- Module3/home_work/05_hw.py
+++ Module3/home_work/05_hw.py
@@ -1,13 +1,23 @@
 # Дан список имен.
 # Найдите самое длинное имя, если таких имен несколько - выведи любое их них
 
-names = ["Иван", "Ирина", "Вячеслав", "Василий", "Петр"]
+names = ["Иван", "Ирина", "Сергей", "Василий", "Петр", "Дмитрий"]
 
-names = ["Иван", "Ирина", "Вячеслав", "Василий", "Петр"]
-index = len(names[0])
+def Max_dlina(names):
+    dlina = len(names[0])
+    element = names[0]
+    for name in names:
+        if dlina < len(name):
+            dlina = len(name)
+            element = name
+    return dlina
+
+Max_dlina = Max_dlina(names)
+long_names = []
 for name in names:
-    if index < len(name):
-        index = len(name)
+    if len(name) == Max_dlina:
+        long_names.append(name)
 
-print(index) 
+print(long_names)
+
 # TODO: your code here
