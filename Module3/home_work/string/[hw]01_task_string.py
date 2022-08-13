2eb2f1e5d4d6d5e8962adbd1317644d87261284e
pts1701 <110337313+pts1701@users.noreply.github.com>
2022-08-13 12:58:11 +0300

    Update [hw]01_task_string.py

--- Module3/home_work/string/[hw]01_task_string.py
+++ Module3/home_work/string/[hw]01_task_string.py
@@ -1,4 +1,20 @@
 # Дан произвольный текст. Каких символов больше, точек "." или запятых ","?
 # Если точек и запятых одинаково - выведите "одинаково"
 
-text = ...
+text = input("Input text:  ")
+stop=[]
+comma=[]
+for char in text:
+    if char == ".":
+        stop.append(char)
+    if char == ",":
+        comma.append(char)
+print("Количество точек = ", len(stop))
+print("Количество запятых = ", len(comma))
+
+if len(stop) > len(comma):
+    print("Точек больше")
+elif len(stop) < len(comma):
+    print("Запятых больше")
+else:
+    print("Запятых и точек одинаковое колличество")
