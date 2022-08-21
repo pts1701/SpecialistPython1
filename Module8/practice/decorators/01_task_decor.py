5d07aa56e0b82a8fb8fe0284c0f9aa033c843f7c
pts1701 <110337313+pts1701@users.noreply.github.com>
2022-08-21 14:43:54 +0300

    Update 01_task_decor.py

--- Module8/practice/decorators/01_task_decor.py
+++ Module8/practice/decorators/01_task_decor.py
@@ -7,3 +7,17 @@
 # *23*
 # ****
 # (кол-во звездочек зависит от длины возвращаемого значения)
+def decor(func):
+    def wrapper():
+        result = func()
+        n = len(result) + 4
+        print('*' * n)
+        print('*', result, '*')
+        print('*' * n)
+    return wrapper
+
+@decor
+def fun():
+    return input('Input smth:')
+
+fun()
